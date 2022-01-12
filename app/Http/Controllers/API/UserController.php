1de55620270419b71976ffed1df0e50c25b59200
gonexwind <fikky.ardi15@gmail.com>
2022-01-12 07:39:52 +0700

    logout api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -84,4 +84,10 @@ class UserController extends Controller
             ], 'Authenticated Failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+        return ResponseFormatter::success($token,'Token Revoked');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('login', [UserController::class, 'login']);
     Route::post('register', [UserController::class, 'register']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('logout', [UserController::class, 'logout']);
+    });
 });
